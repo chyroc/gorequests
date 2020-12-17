ddd352300e3e83c28f83d7abac563a6e2fa770b7
chyroc <chyroc@qq.com>
2020-12-17 14:09:39 +0800

    feat: same cookie-file has same session instance

--- session.go
+++ session.go
@@ -1,6 +1,8 @@
 package gorequests
 
 import (
+	"sync"
+
 	cookiejar "github.com/juju/persistent-cookiejar"
 )
 
@@ -16,7 +18,29 @@ func (r *Session) New(method, url string) *Request {
 	return req
 }
 
+var sessionLock sync.Mutex
+var sessionMap map[string]*Session
+
+func init() {
+	sessionMap = map[string]*Session{}
+}
+
+// same cookie-file has same session instance
 func NewSession(cookiefile string) *Session {
+	sessionLock.Lock()
+	defer sessionLock.Unlock()
+
+	v := sessionMap[cookiefile]
+	if v != nil {
+		return v
+	}
+
+	v = newSession(cookiefile)
+	sessionMap[cookiefile] = v
+	return v
+}
+
+func newSession(cookiefile string) *Session {
 	jar, err := cookiejar.New(&cookiejar.Options{
 		Filename: cookiefile,
 	})
