0f4c33285ff0de9624914d4c3b82f81784a1a012
chyroc <chyroc@qq.com>
2021-04-12 20:19:47 +0800

    with query struct

--- requests.go
+++ requests.go
@@ -11,6 +11,7 @@ import (
 	"net/http"
 	"net/url"
 	"reflect"
+	"strconv"
 	"strings"
 	"sync"
 	"time"
@@ -127,6 +128,29 @@ func (r *Request) WithQuerys(kv map[string]string) *Request {
 	}
 	for k, v := range kv {
 		r.querys[k] = append(r.querys[k], v)
+	}
+	return r
+}
+
+// query-struct
+func (r *Request) WithQueryStruct(v interface{}) *Request {
+	if r.err != nil {
+		return r
+	}
+
+	r.reqlock.Lock()
+	defer r.reqlock.Unlock()
+	if r.cachedurl != "" {
+		r.err = fmt.Errorf("[gorequests] already send request, cannot add query param")
+		return r
+	}
+	kv, err := queryToMap(v)
+	if err != nil {
+		r.err = err
+		return r
+	}
+	for k, v := range kv {
+		r.querys[k] = append(r.querys[k], v...)
 	}
 	return r
 }
@@ -436,3 +460,100 @@ func (r *Request) ctx() context.Context {
 	}
 	return context.Background()
 }
+
+func queryToMap(v interface{}) (map[string][]string, error) {
+	ss, err := getQueryToMapKeys(v)
+	if err != nil {
+		return nil, err
+	} else if len(ss) == 0 {
+		return map[string][]string{}, nil
+	}
+
+	vv := reflect.ValueOf(v)
+	if vv.Kind() == reflect.Ptr {
+		vv = vv.Elem()
+	}
+
+	vals := map[string][]string{}
+	for _, s := range ss {
+		vals[s.query], err = toStringList(vv.Field(s.idx))
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return vals, nil
+}
+
+func toStringList(v reflect.Value) ([]string, error) {
+	switch v.Kind() {
+	case reflect.String:
+		return []string{v.String()}, nil
+	case reflect.Bool:
+		if v.Bool() {
+			return []string{"true"}, nil
+		}
+		return []string{"false"}, nil
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+		return []string{strconv.FormatInt(v.Int(), 10)}, nil
+	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+		return []string{strconv.FormatUint(v.Uint(), 10)}, nil
+	case reflect.Array, reflect.Slice:
+		res := []string{}
+		for j := 0; j < v.Len(); j++ {
+			x, err := toStringList(v.Index(j))
+			if err != nil {
+				return nil, err
+			}
+			res = append(res, x...)
+		}
+		return res, nil
+	}
+
+	return nil, fmt.Errorf("invalid value: %s", v.Kind())
+}
+
+func getQueryToMapKeys(v interface{}) ([]s, error) {
+	origin := reflect.TypeOf(v)
+	v, ok := queryToMapKeys.Load(origin)
+	if ok {
+		return v.([]s), nil
+	}
+
+	vt := origin
+	// vv := reflect.ValueOf(v)
+	if vt.Kind() == reflect.Ptr {
+		vt = vt.Elem()
+		// vv = vv.Elem()
+	}
+	if vt.Kind() != reflect.Struct {
+		return nil, fmt.Errorf("need strcut, but got %s", vt.Kind())
+	}
+
+	ss := []s{}
+	for i := 0; i < vt.NumField(); i++ {
+		itemT := vt.Field(i)
+		// itemV := vv.Field(i)
+
+		queryKey := itemT.Tag.Get("query")
+		if queryKey == "" {
+			continue
+		}
+		// itemV.String()
+		ss = append(ss, s{
+			idx:   i,
+			query: queryKey,
+		})
+	}
+
+	queryToMapKeys.Store(origin, ss)
+
+	return ss, nil
+}
+
+type s struct {
+	idx   int
+	query string
+}
+
+var queryToMapKeys sync.Map
